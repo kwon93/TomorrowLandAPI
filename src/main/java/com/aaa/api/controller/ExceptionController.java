64da5c65590826922cdea8e4a6f44e2ef4dfca33
kwon <93umsongaji@gmail.com>
2024-07-29 22:56:36 +0900

    refactor: exception controller 추상화 진행

--- src/main/java/com/aaa/api/controller/ExceptionController.java
+++ src/main/java/com/aaa/api/controller/ExceptionController.java
@@ -15,30 +15,38 @@ public class ExceptionController {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ExceptionResponse methodArgsHandler(MethodArgumentNotValidException e){
-        ExceptionResponse response = ExceptionResponse.builder()
-                .code("400")
-                .errorMessage("잘못된 요청입니다.")
-                .build();
-
-        for (FieldError fieldError : e.getFieldErrors()) {
-            response.addValidation(fieldError.getField(), fieldError.getDefaultMessage());
-        }
+    public ExceptionResponse methodArgsHandler(MethodArgumentNotValidException exception){
+        ExceptionResponse response = buildDefaultExceptionResponse();
+        addValidationToResponse(exception, response);
         return response;
     }
 
-
     @ResponseBody
     @ExceptionHandler(TomorrowException.class)
-    public ResponseEntity<ExceptionResponse> exceptionHandler(TomorrowException e){
+    public ResponseEntity<ExceptionResponse> exceptionHandler(TomorrowException tomorrowException){
+        ExceptionResponse response = buildTomorrowExceptionResponse(tomorrowException);
+        return ResponseEntity.status(tomorrowException.statusCode()).body(response);
+    }
 
-        ExceptionResponse response = ExceptionResponse.builder()
-                .code(String.valueOf(e.statusCode()))
-                .errorMessage(e.getMessage())
-                .validation(e.getValidation())
-                .userMessage(e.getUserMessage())
+    private static ExceptionResponse buildDefaultExceptionResponse() {
+        return ExceptionResponse.builder()
+                .code("400")
+                .errorMessage("잘못된 요청입니다.")
                 .build();
+    }
 
-        return ResponseEntity.status(e.statusCode()).body(response);
+    private static ExceptionResponse buildTomorrowExceptionResponse(TomorrowException tomorrowException) {
+        return ExceptionResponse.builder()
+                .code(String.valueOf(tomorrowException.statusCode()))
+                .errorMessage(tomorrowException.getMessage())
+                .validation(tomorrowException.getValidation())
+                .userMessage(tomorrowException.getUserMessage())
+                .build();
+    }
+
+    private static void addValidationToResponse(MethodArgumentNotValidException e, ExceptionResponse response) {
+        for (FieldError fieldError : e.getFieldErrors()) {
+            response.addValidation(fieldError.getField(), fieldError.getDefaultMessage());
+        }
     }
 }
