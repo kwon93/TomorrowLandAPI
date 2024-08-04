cb88cdff002b744bd618b593c880984ac041c8fe
kwon <93umsongaji@gmail.com>
2024-08-04 21:00:26 +0900

    refactor: CustomUserDetailService

--- src/main/java/com/aaa/api/service/CustomUserDetailsService.java
+++ src/main/java/com/aaa/api/service/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package com.aaa.api.service;
 
 import com.aaa.api.config.security.CustomUserPrincipal;
 import com.aaa.api.domain.Users;
+import com.aaa.api.exception.UserNotFound;
 import com.aaa.api.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,22 +17,32 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class CustomUserDetailsService implements UserDetailsService {
 
+    public static final String USER_INFO = "userInfo";
     private final UsersRepository usersRepository;
     private final RedisTemplate redisTemplate;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Integer userId = (Integer) redisTemplate.opsForHash().get("userInfo", "userId");
-        String userEmail = (String) redisTemplate.opsForHash().get("userInfo", "username");
-        String userRole = (String) redisTemplate.opsForHash().get("userInfo", "userRole");
+        Integer userId = (Integer) getUserInfoFromRedis("userId");
+        String userEmail = (String) getUserInfoFromRedis("username");
+        String userRole = (String) getUserInfoFromRedis("userRole");
 
-        if (userEmail == null || userRole == null){
-            final Users users = usersRepository.findByEmail(username)
-                    .orElseThrow(() -> new UsernameNotFoundException("찾을 수 없는 이메일입니다."));
-
-            return new CustomUserPrincipal(users.getEmail(), users.getRoles().toString(), users.getId());
-        }
+        if (doseUserSignIn(userEmail, userRole)) return setUserPrincipalFromDatabase(username);
 
         return new CustomUserPrincipal(username, userRole, userId.longValue());
+    }
+
+    private CustomUserPrincipal setUserPrincipalFromDatabase(String username) {
+        final Users users = usersRepository.findByEmail(username)
+                .orElseThrow(UserNotFound::new);
+        return new CustomUserPrincipal(users.getEmail(), users.getRoles().toString(), users.getId());
+    }
+
+    private Object getUserInfoFromRedis(String userInfo) {
+        return redisTemplate.opsForHash().get(USER_INFO, userInfo);
+    }
+
+    private static boolean doseUserSignIn(String userEmail, String userRole) {
+        return userEmail == null || userRole == null;
     }
 }
